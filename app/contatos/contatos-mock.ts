b95c4877e6db1e6a3d164ab42820c2fc6aedcdbe
Roosevelt Franklin <roosevelt_franklin@hotmail.com>
2018-01-17 23:31:35 -0200

    Commit inicial

---
+++ app/contatos/contatos-mock.ts
@@ -0,0 +1,9 @@
+import { Contato } from './contato.model';
+
+export const CONTATOS: Contato[ ] = [
+    {id: 1, nome: 'Fulano de Tal', email: 'fulano@email.com', telefone: '(00) 0000-0000'},
+    {id: 2, nome: 'Ciclano Silva', email: 'ciclano@email.com', telefone: '(00) 0000-0000'},
+    {id: 3, nome: 'Clovis Pinto', email: 'clovis@email.com', telefone: '(00) 0000-0000'},
+    {id: 4, nome: 'Tio Flovis', email: 'flovis@email.com', telefone: '(00) 0000-0000'},
+    {id: 5, nome: 'Sogro Pança', email: 'sogro@email.com', telefone: '(00) 0000-0000'},
+];
